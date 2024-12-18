c85a1ad7bb86b9e3bf68568dab6d5a020e2af2ab
nguyenlh6 <nguyenlehoang361@gmail.com>
2024-12-18 19:08:58 +0700

    mouseless

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,8 +40,9 @@
     bat
     delta
     i3-resurrect
-    dnsutil
-    telnet
+    dnsutils
+    inetutils
+    autokey
 
     # font
     # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
@@ -57,6 +58,8 @@
     ".config/i3".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/i3";
     ".config/alacritty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/alacritty";
     ".config/rofi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/rofi";
+    ".config/mouseless/config.yaml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/mouseless/config.yaml";
+    ".keynavrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/mouseless/keynavrc";
     ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/git/gitconfig";
     ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/zsh/zshrc";
     ".p10k.zsh".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/zsh/p10k.zsh";
